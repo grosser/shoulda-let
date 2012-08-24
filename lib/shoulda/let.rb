012e7900fa6bb2c3d4559d08c1a9a7519d338b38
grosser <grosser.michael@gmail.com>
2012-08-23 21:18:42 -0700

    cleanup and docs

--- lib/shoulda/let.rb
+++ lib/shoulda/let.rb
@@ -3,6 +3,8 @@ require "shoulda/context"
 
 context_class = (Shoulda::VERSION >= "3.0.0" ? Shoulda::Context::Context : Shoulda::Context)
 context_class.class_eval do
+  # setup method used outside of should blocks
+  # let(:foo){ "Foo" }
   def let(name, &block)
     @let ||= {}
     if block
@@ -13,8 +15,9 @@ context_class.class_eval do
   end
 end
 
-instance_class = (Shoulda::VERSION >= "3.0.0" ? Test::Unit::TestCase : Shoulda::InstanceMethods)
-instance_class.class_eval do
+Test::Unit::TestCase.class_eval do
+  # instance method used inside of should block
+  # assert_equal @result, let(:foo)
   def let(name)
     @let ||= {}
     @let.fetch(name) do
